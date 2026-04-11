80dd9b9f08d755fe8a5e7fcc60200fedf30e0bb5
Christian Ishimwe <christianishimwe2003@gmail.com>
2026-04-11 14:59:41 -0400

    feat: tested the agent's ability to debug. I intentionally chnaged the precedence of + operator in the calculator to 3 instead of 1. this breaks the code, since for 3 + 7 * 2 the result now becomes 20 instead of 17, and told the agent to fix the code, which it did very well

--- aiagent/prompt.py
+++ aiagent/prompt.py
@@ -9,4 +9,7 @@ When a user asks a question or makes a request, make a function call plan. you c
 - Write or overwrite files with content
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+Make sure to provide a helpful feedback message after performing user's request. If the users request included a question, or if the user wanted feedback of their request, you should also 
+provide that at the end.
+If you happen to edit a file, please also say in your final message what edits you made, and why you specifically made those edits.
 """
